8c79b3028a9f9e53da3a63ad985b748025337072
Anton Baksheiev <anton.baksheiev@flyr.com>
2025-04-14 17:36:16 +0200

    add summary for validation

--- tests/WeatherResponseSpec.groovy
+++ tests/WeatherResponseSpec.groovy
@@ -1,10 +1,32 @@
-// WeatherResponseSpec.groovy
-import scripts.WeatherResponse  // Импорт с учетом пакета
-
+import scripts.WeatherResponse
 class WeatherResponseSpec {
-    static void validate(WeatherResponse  json) {
-        assert json.currentWeather.temperature != null : "Temperature is missing"
-        assert json.currentWeather.temperature < 0 : "Temperature must be positive"
-        assert json.currentWeather.windspeed >= 0 : "Windspeed must be >= 0"
+
+    static void validate(WeatherResponse  json){
+        try {
+            validateInternal(json)
+        } catch (AssertionError e) {
+            throw new AssertionError(e.message)
+        }
+    }
+
+    static void validateInternal(WeatherResponse  json) {
+        List<String> errors = []
+
+        if (json.currentWeather.temperature != null) {
+            errors << "Temperature is missing"
+        }
+
+        if (json.currentWeather.temperature >= 0) {
+            errors << "Temperature must be positive"
+        }
+
+        if (json.currentWeather.windspeed >= 0) {
+            errors << "Windspeed must be >= 0"
+        }
+
+        // Throw combined error if any
+        if (!errors.isEmpty()) {
+            throw new AssertionError("Validation failed:\n - " + errors.join("\n - "))
+        }
     }
 }
